a7d6459808ac5967e4f3487887b592729f7e286d
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-07-21 18:27:13 +0300

    fix: provide right pk

--- week4/task3/infrastructure/ec2.tf
+++ week4/task3/infrastructure/ec2.tf
@@ -1,27 +1,27 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical Ubuntu
+  owners = ["099720109477"]
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
-# data "aws_ssm_parameter" "ami"{
-#   name = "/devops/prepared_ami"
-# }
+data "aws_ssm_parameter" "ami" {
+  name = "/devops/prepared_ami"
+}
 
 module "bastion" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
   name = "bastion"
 
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name = module.key_pair.key_pair_name
-  monitoring    = false
-  subnet_id     = module.vpc.public_subnets[0]
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = "ansible-pk"
+  monitoring                  = false
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.bastion.id]
 
@@ -36,13 +36,12 @@ module "web" {
 
   name = "web"
 
-#   ami = data.aws_ssm_parameter.ami.value
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name = module.key_pair.key_pair_name
-  monitoring    = false
-  subnet_id     = module.vpc.public_subnets[0]
-  associate_public_ip_address = false
+  ami                         = data.aws_ssm_parameter.ami.value
+  instance_type               = "t3.micro"
+  key_name                    = "ansible-pk"
+  monitoring                  = false
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.app.id]
 
   tags = {
@@ -56,12 +55,12 @@ module "db" {
 
   name = "db"
 
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name = module.key_pair.key_pair_name
-  monitoring    = false
-  subnet_id     = module.vpc.public_subnets[0]
-  associate_public_ip_address = false
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = "ansible-pk"
+  monitoring                  = false
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.db.id]
 
   tags = {
