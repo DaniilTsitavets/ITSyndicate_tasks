e045eccf6d3360e70c87868a2165f9eaf616be84
Greed <d.tsitavets@gmail.com>
2025-08-13 19:29:16 +0300

    feature: week13 (#20)
    * feature: add infra for tasks
    
    * feature: add crd for rules

---
+++ week13/task1/infra/sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "eks_nodes" {
+  name   = "eks-node-sg"
+  vpc_id = aws_vpc.task3_vpc.id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
